39c2d2d6699c90f46bc831387ebd5d4a75decd82
nutchapon <nutchapon2003@gmail.com>
2023-09-17 18:12:16 +0700

    add change styles

--- app/src/main/java/com/example/numberguessinggame/MainActivity.kt
+++ app/src/main/java/com/example/numberguessinggame/MainActivity.kt
@@ -3,18 +3,27 @@ package com.example.numberguessinggame
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,96 +54,132 @@ fun NumberGuessingGameLayout() {
     val randomNumber = remember { mutableStateOf(Random.nextInt(1, 1001)) }
     val guess = guessInput.toIntOrNull()
 
-    Column(
-        modifier = Modifier.padding(40.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = stringResource(R.string.guess_number),
-            modifier = Modifier
-                .padding(bottom = 16.dp)
-                .align(alignment = Alignment.CenterHorizontally),
-            style = TextStyle(
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold
-            )
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize().alpha(0.8f) ,
+
         )
-        EditNumberField(
-            value = guessInput,
-            onValueChanged = { guessInput = it },
-            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
-        )
-        Button(
-            onClick = {
-                message = if (guess != null) {
-                    when {
-                        guess < randomNumber.value -> {
-                            if (guess < randomNumber.value - 50) {
-                                "Way too low! Try a much higher number."
-                            } else {
-                                "Too low! Try a higher number."
-                            }
-                        }
-                        guess > randomNumber.value -> {
-                            if (guess > randomNumber.value + 50) {
-                                "Way too high! Try a much lower number."
-                            } else {
-                                "Too high! Try a lower number."
-                            }
-                        }
-                        else -> {
-                            isGameOver = true
-                            "Congratulations! You guessed it!"
-                        }
-                    }
-                } else {
-                    "Please enter a valid number."
-                }
-            },
+
+        Column(
             modifier = Modifier
-                .padding(bottom = 16.dp)
-                .size(width = 200.dp, height = 60.dp)
+                .fillMaxSize()
+                .padding(40.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Text(text = stringResource(R.string.check_guess),
+            Text(
+                text = stringResource(id = R.string.app_name),
+                modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
                 style = TextStyle(
-                    fontSize = 18.sp
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.ExtraBold
                 )
             )
-        }
 
-        if (isGameOver) {
-            AlertDialog(
-                onDismissRequest = {
-                    isGameOver = false
-                    guessInput = ""
-                    message = ""
-                    randomNumber.value = Random.nextInt(1, 1001)
-                },
-                title = { Text(text = "Congratulations!") },
-                text = { Text(text = "You guessed it!") },
-                confirmButton = {
-                    Button(
-                        onClick = {
-                            isGameOver = false
-                            guessInput = ""
-                            message = ""
-                            randomNumber.value = Random.nextInt(1, 1001)
-                        },
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text(text = "Play Again")
-                    }
-                }
+            Text(
+                text = stringResource(R.string.detail),
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .align(alignment = Alignment.CenterHorizontally),
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
             )
+
+            EditNumberField(
+                value = guessInput,
+                onValueChanged = { guessInput = it },
+                modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
+            )
+
+            Button(
+                onClick = {
+                    message = if (guess != null) {
+                        when {
+                            guess < randomNumber.value -> {
+                                if (guess < randomNumber.value - 50) {
+                                    "Way too low! \nTry a much higher number."
+                                } else {
+                                    "Too low! \nTry a higher number."
+                                }
+                            }
+                            guess > randomNumber.value -> {
+                                if (guess > randomNumber.value + 50) {
+                                    "Way too high! \nTry a much lower number."
+                                } else {
+                                    "Too high! \nTry a lower number."
+                                }
+                            }
+                            else -> {
+                                isGameOver = true
+                                "Congratulations! You guessed it!"
+                            }
+                        }
+                    } else {
+                        "Please enter a valid number."
+                    }
+                },
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .size(width = 200.dp, height = 60.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.White, // Text color
+                    containerColor = Color(0xFFFF9000)// Background color
+                )
+            ) {
+                Text(text = stringResource(R.string.check_guess),
+                    style = TextStyle(
+                        fontSize = 18.sp
+                    )
+
+                )
+            }
+
+            if (isGameOver) {
+                AlertDialog(
+                    onDismissRequest = {
+                        isGameOver = false
+                        guessInput = ""
+                        message = ""
+                        randomNumber.value = Random.nextInt(1, 1001)
+                    },
+                    title = { Text(text = "Congratulations!") },
+                    text = { Text(text = "You guessed it!") },
+                    confirmButton = {
+                        Button(
+                            onClick = {
+                                isGameOver = false
+                                guessInput = ""
+                                message = ""
+                                randomNumber.value = Random.nextInt(1, 1001)
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(text = "Play Again")
+                        }
+                    }
+                )
+            }
+            Text(
+                text = message,
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    color = Color.White
+                )
+
+            )
+            Spacer(modifier = Modifier.height(150.dp))
         }
-        Text(
-            text = message,
-            style = MaterialTheme.typography.labelMedium
-        )
-        Spacer(modifier = Modifier.height(150.dp))
     }
 }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -152,7 +197,7 @@ fun EditNumberField(
             Text(
                 text = stringResource(R.string.enter_guess),
                 style = TextStyle(
-                    fontSize = 18.sp,
+                    fontSize = 20.sp,
                     fontWeight = FontWeight.Bold,
                     color = Color.Gray
                 )
@@ -163,7 +208,7 @@ fun EditNumberField(
             imeAction = ImeAction.Done
         ),
         textStyle = TextStyle(
-            fontSize = 18.sp,
+            fontSize = 20.sp,
             fontWeight = FontWeight.Normal,
             color = Color.Black
         )
